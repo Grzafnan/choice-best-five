fcd7e6867f1aa35125c24571471756dfa1d5f6f4
Grzafnan <Grzafnan>
2022-08-22 14:38:13 +0600

    fixed button disabled color

--- js/app.js
+++ js/app.js
@@ -1,26 +1,26 @@
 let count = 0;
-const selectBtn = document.getElementsByClassName('select-btn');
+let selectBtn = document.getElementsByClassName('select-btn');
 
 for (selector of selectBtn) {
   selector.addEventListener('click', function (event) {
     count++;
-    const playerName = event.target.parentNode.parentNode.childNodes[1].innerText;
-    const selected = event.target.setAttribute('disabled', '');
+    let playerName = event.target.parentNode.parentNode.childNodes[1].innerText;
+    let selected = event.target.setAttribute('disabled', '');
 
     if (count > 5) {
-      const selected = event.target.removeAttribute("disabled");
+      let selected = event.target.removeAttribute('disabled');
       alert("You Can't Added More Then 5 Player");
       return;
     }
 
-    const selectedPlayer = document.getElementById('selected-total');
-    const totalSelectedPlayer = parseFloat(selectedPlayer.innerText);
+    let selectedPlayer = document.getElementById('selected-total');
+    let totalSelectedPlayer = parseFloat(selectedPlayer.innerText);
     selectedPlayer.innerText = count;
 
 
-    const mainContainer = document.getElementById("body-container");
+    let mainContainer = document.getElementById("body-container");
 
-    const bodyContainer = document.createElement("tr");
+    let bodyContainer = document.createElement("tr");
     bodyContainer.innerHTML = `
        <th scope="row">${count}</th>
        <td>  </td>
